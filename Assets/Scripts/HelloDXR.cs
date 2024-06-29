bb9810416ef4c6c5f9a2a532208d9217d23568a1
sugita-shumpei <shumpei.sugita@fj.ics.keio.ac.jp>
2024-06-29 21:10:30 +0900

    add basic material

--- Assets/Scripts/HelloDXR.cs
+++ Assets/Scripts/HelloDXR.cs
@@ -16,26 +16,20 @@ public class HelloDXR : MonoBehaviour
 
     private Matrix4x4 _prevViewMatrix = Matrix4x4.identity;
     private Matrix4x4 _prevProjMatrix = Matrix4x4.identity;
+    const string kTargetShaderPass = "HelloDXR";
+    const string kRayGenShaderName = "RayGenShaderForTest";
     private int _resIdxRenderTarget = 0;
     private int _resIdxWorld = 0;
     private bool _dirtyAS = false;
-    private bool _updateAS = false;
 
-    private void Awake()
+    private void OnEnable()
     {
-        if (rayTracingShader == null)
-        {
-            return;
-        }
-        _resIdxRenderTarget    = Shader.PropertyToID("RenderTarget");
-        _resIdxWorld           = Shader.PropertyToID("World");
+        _resIdxRenderTarget = Shader.PropertyToID("RenderTarget");
+        _resIdxWorld = Shader.PropertyToID("World");
         _accelerationStructure = new RayTracingAccelerationStructure();
         _renderTarget = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
         _renderTarget.enableRandomWrite = true;
         _renderTarget.Create();
-    }
-    private void OnEnable()
-    {
         BuildAccelerationStructure();
     }
 
@@ -52,66 +46,6 @@ public class HelloDXR : MonoBehaviour
             _renderTarget = null;
         }
     }
-    public void MarkDirty()
-    {
-        _dirtyAS = true;
-    }
-
-    void BuildAccelerationStructure()
-    {
-        var flags = new List<RayTracingSubMeshFlags>();
-        flags.Capacity = 1;
-        for (int i = 0; i < flags.Capacity; i++)
-        {
-            flags.Add(RayTracingSubMeshFlags.Enabled);
-        }
-        // レイトレーシングの対象となるRendererを取得
-        var renderers = FindObjectsByType<Renderer>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
-        // それまでのInstanceをクリア
-        _accelerationStructure.ClearInstances();
-        foreach (var renderer in renderers)
-        {
-            // レイトレーシングの対象となるRendererを登録
-            _accelerationStructure.AddInstance(renderer, flags.ToArray());
-        }
-        // ビルド
-        _accelerationStructure.Build();
-    }
-
-    bool UpdateResources(int width_, int height_)
-    {
-        bool updateAS = _updateAS;
-        bool isDirty = _dirtyAS;
-        var viewMatrix = Camera.main.worldToCameraMatrix;
-        var projMatrix = Camera.main.projectionMatrix;
-        if (_renderTarget)
-        {
-            if (_renderTarget.width != width_ || _renderTarget.height != height_)
-            {
-                _renderTarget.Release();
-                _renderTarget.width = width_;
-                _renderTarget.height = height_;
-                _renderTarget.Create();
-            }
-        } else if (_renderTarget == null)
-        {
-            _renderTarget = new RenderTexture(width_, height_, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-            _renderTarget.enableRandomWrite = true;
-            _renderTarget.Create();
-        }
-        if (viewMatrix != _prevViewMatrix || projMatrix != _prevProjMatrix)
-        {
-            _prevViewMatrix = viewMatrix;
-            _prevProjMatrix = projMatrix;
-        }
-        if (updateAS)
-        {
-            BuildAccelerationStructure();
-            _updateAS = false;
-            isDirty   = true;
-        }
-        return isDirty;
-    }
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if (rayTracingShader == null)
@@ -120,23 +54,81 @@ public class HelloDXR : MonoBehaviour
         }
         else
         {
-            if (!UpdateResources(source.width, source.height))
+            UpdateResources(source.width, source.height);
             {
-                // レイトレーシングシェーダーのパスを設定(必須)
-                // ここではPathTraceという名前のパスを使用
-                // この名前はAccelerationStructureのビルド時に入力したRendererのMaterialのパス名に対応している
-                // もし, パス名が含まれていない場合, そのマテリアルではシェーダが実行されないので注意
-                rayTracingShader.SetShaderPass("HelloDXR");
+                rayTracingShader.SetShaderPass(kTargetShaderPass);
                 rayTracingShader.SetTexture(_resIdxRenderTarget, _renderTarget);
                 rayTracingShader.SetAccelerationStructure(_resIdxWorld, _accelerationStructure);
-                // レイトレーシングを実行
-                rayTracingShader.Dispatch("RayGenShaderForTest", Screen.width, Screen.height, 1, Camera.main);
+                rayTracingShader.Dispatch(kRayGenShaderName, Screen.width, Screen.height, 1, Camera.main);
                 Graphics.Blit(_renderTarget, destination);
             }
-            else
-            {
-                Graphics.Blit(source, destination);
-            }
         }
+    }
+    void Resize(int width_, int height_, bool clear_ = false)
+    {
+        if (_renderTarget.width == width_ && _renderTarget.height == height_)
+        {
+            _renderTarget.Release();
+            _renderTarget.width = width_;
+            _renderTarget.height = height_;
+            _renderTarget.Create();
+            return;
+        }
+        return;
+    }
+
+    public void MarkDirty()
+    {
+        _dirtyAS = true;
+    }
+
+    void BuildAccelerationStructure()
+    {
+        var flags = new List<RayTracingSubMeshFlags>(new RayTracingSubMeshFlags[1] { RayTracingSubMeshFlags.Enabled });
+        var renderers = FindObjectsByType<Renderer>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
+        _accelerationStructure.ClearInstances();
+        foreach (var renderer in renderers)
+        {
+            _accelerationStructure.AddInstance(renderer, flags.ToArray());
+        }
+        _accelerationStructure.Build();
+        rayTracingShader.SetAccelerationStructure(_resIdxWorld, _accelerationStructure);
+    }
+    bool UpdateCamera()
+    {
+        var camera = gameObject.GetComponent<Camera>();
+        if (camera == null) { return false; }
+        var viewMatrix = camera.worldToCameraMatrix;
+        var projMatrix = camera.projectionMatrix;
+        if (viewMatrix != _prevViewMatrix || projMatrix != _prevProjMatrix)
+        {
+            _prevViewMatrix = viewMatrix;
+            _prevProjMatrix = projMatrix;
+            return true;
+        }
+        return false;
+    }
+    bool UpdateAccelerationStructures()
+    {
+        if (_dirtyAS)
+        {
+            BuildAccelerationStructure();
+            _dirtyAS = false;
+            return true;
+        }
+        return false;
+    }
+    void UpdateResources(int width_, int height_)
+    {
+        bool updateFrame = false;
+        if (UpdateAccelerationStructures())
+        {
+            updateFrame = true;
+        }
+        if (UpdateCamera())
+        {
+            updateFrame = true;
+        }
+        Resize(width_, height_, updateFrame);
     }
 }
