b8139bbfc5bebcab8c25436dcf3fd2453fab1a21
sugita-shumpei <shumpei.sugita@fj.ics.keio.ac.jp>
2024-06-30 03:09:36 +0900

    fix

--- Assets/Scripts/SimplePathTracer.cs
+++ Assets/Scripts/SimplePathTracer.cs
@@ -308,7 +308,7 @@ public class SimplePathTracer : MonoBehaviour
     }
     void BuildAccelerationStructure()
     {
-        if (_accelerationStructure != null)
+        if (_accelerationStructure == null)
         {
             CreateAccelerationStructure();
         }
